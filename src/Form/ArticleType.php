ffca3fe881d41cb91613eac670f3875d98ea7b24
Zeller <matheo.zeller@gmail.com>
2021-07-26 17:59:14 +0200

    :tada: begin blog project

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'help'  => '50 caractères maximum'
+            ])
+            ->add('chapo', TextType::class, [
+                'help'  => '60 caractères maximum'
+            ])
+            ->add('content', TextType::class, [
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
